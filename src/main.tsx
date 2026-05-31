d988717a10ceda830d4cffab6d3ce827e3ec52fb
bachrun <bachrunalichqodrah@gmail.com>
2026-05-31 11:00:10 +0700

    fix: missing StrictMode import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Component, type ReactNode, type ErrorInfo } from 'react'
+import { StrictMode, Component, type ReactNode, type ErrorInfo } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
